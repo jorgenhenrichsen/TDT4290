dd21f68b733ec142fc07cf446bc65b491cf8d021
Silje Riseng <silje.riseng@gmail.com>
2017-09-11 13:31:12 +0200

    added a simple login page - unstyled

--- athlitikos/athlitikos/urls.py
+++ athlitikos/athlitikos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
     url(r'^admin/', admin.site.urls),
 ]
