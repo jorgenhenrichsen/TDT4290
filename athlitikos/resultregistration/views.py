733e6eb49cf9f16c560f899a23101786faf8b230
noah <noah22sun@gmail.com>
2017-09-18 04:06:59 -0700

    Redirect to login page

--- athlitikos/resultregistration/views.py
+++ athlitikos/resultregistration/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 # TODO: This view should be login-only
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'home.html')
+
 
