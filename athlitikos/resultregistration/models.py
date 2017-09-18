f81d561db5c5e6180b96f691314f9ad99a7cb261
TorMagne <tor.magne@kippersund.no>
2017-09-18 11:00:23 +0200

    Made the beginning of a result model

--- athlitikos/resultregistration/models.py
+++ athlitikos/resultregistration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Result(models.Model):
+    resultID = models.IntegerField(primary_key=True)
+    total = models.IntegerField()
+    points = models.IntegerField()
+    points_veteran = models.IntegerField()
