d74d1d517a45f096cae0863f41da1a9f580ddc24
jorgenhenrichsen <jh.henrichs@gmail.com>
2017-09-19 13:27:12 +0200

    Added DateTimeSelect field to LifterForm

--- athlitikos/resultregistration/forms.py
+++ athlitikos/resultregistration/forms.py
@@ -7,3 +7,7 @@ class LifterForm(forms.ModelForm):
     class Meta:
         model = Lifter
         fields = ('first_name', 'last_name', 'birth_date')
+
+    def __init__(self, *args, **kwargs):
+        super(LifterForm, self).__init__(*args, **kwargs)
+        self.fields['birth_date'].widget = forms.widgets.SelectDateWidget()
