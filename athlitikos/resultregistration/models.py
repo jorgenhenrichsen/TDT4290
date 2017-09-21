acfb073eaa2774ac996d2385414f24ba9477be04
jorgenhenrichsen <jh.henrichs@gmail.com>
2017-09-21 09:06:50 +0200

    Moved enums to seperate file

--- athlitikos/resultregistration/models.py
+++ athlitikos/resultregistration/models.py
@@ -1,28 +1,4 @@
 from django.db import models
-from enum import Enum
-import inspect
-
-
-class ChoiceEnum(Enum):
-
-    @classmethod
-    def choices(cls):
-        members = inspect.getmembers(cls, lambda m: not(inspect.isroutine(m)))
-        properties = [m for m in members if not(m[0][:2] == '__')]
-        choices = tuple([(str(p[1].value), p[0]) for p in properties])
-        return choices
-
-
-class JudgeLevel(ChoiceEnum):
-    Level0 = 0
-    Level1 = 1
-    Level2 = 2
-    Level3 = 3
-
-
-class Gender(ChoiceEnum):
-    male = 'M'
-    female = 'K'
 
 
 class Person(models.Model):
