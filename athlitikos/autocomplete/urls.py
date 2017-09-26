bb5521b302fab50069fa4cc0cbfb5750dea5b8f8
NoraTomas <noratomas@hotmail.com>
2017-09-26 15:40:24 +0200

    added method to check if request is POST

--- athlitikos/autocomplete/urls.py
+++ athlitikos/autocomplete/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^autocomplete/$', views.autocomplete, name='login')
+    url(r'^autocomplete/$', views.autocomplete, name='login'),
+    url(r'^search/$', views.search_club_names)
 ]
--- athlitikos/autocomplete/views.py
+++ athlitikos/autocomplete/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from athlitikos.resultregistration.models import Lifter
 
 def autocomplete(request):
     return render(request, 'base.html')
+
+def search_club_names(request):
+
+    if request.method == 'POST':
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+
+    #clubs = Club.objects.filter()
+
