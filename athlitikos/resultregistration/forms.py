5db72733383ff598af94e1da577f0aaa8c46febd
Silje Riseng <silje.riseng@gmail.com>
2017-10-03 11:19:33 +0200

    collated form working, not styled

--- athlitikos/resultregistration/forms.py
+++ athlitikos/resultregistration/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from .models import Lifter, Judge, Staff, Group
+from .models import *
 from django.utils import timezone
+from betterforms.multiform import MultiModelForm
+
 
 YEAR_CHOICES = [y for y in range(1900, timezone.now().year)]
 
@@ -38,7 +40,41 @@ class StaffForm(forms.ModelForm):
         self.fields['birth_date'].widget = forms.widgets.SelectDateWidget(years=YEAR_CHOICES)
 
 
-class ResultRegistrationForm(forms.ModelForm):
+# For the resultregistration page
+class CompetitonForm(forms.ModelForm):
+    class Meta:
+        model = Competition
+        fields = '__all__'
+
+
+class ClubForm(forms.ModelForm):
+    class Meta:
+        model = Club
+        fields = '__all__'
+
+class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = '__all__'
+        # May have to remove the foreign key models
+
+class ResultForm(forms.ModelForm):
+    class Meta:
+        model = Result
+        exclude = ['resultID']
+        # https://docs.djangoproject.com/en/1.11/topics/forms/modelforms/#selecting-the-fields-to-use
+
+
+class MoveAttemptForm(forms.ModelForm):
+    class Meta:
+        model = MoveAttempt
+        fields = '__all__'
+
+class CompiledResultRegistrationForm(MultiModelForm):
+    form_classes = {
+        'competition-details': CompetitonForm,
+        'administrative-details': GroupForm,
+        'lifter': LifterForm,
+        'move-attempt': MoveAttemptForm,
+        'results': ResultForm,
+    }
