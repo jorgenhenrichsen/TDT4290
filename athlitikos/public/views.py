ac2c050e0bce69cc1ff323c515e1e5704a079d68
jorgenhenrichsen <jh.henrichs@gmail.com>
2017-10-03 12:51:34 +0200

    Lifter id is added as a value to the form

--- athlitikos/public/views.py
+++ athlitikos/public/views.py
@@ -17,10 +17,9 @@ def search(request):
     clubs = Club.objects.all()
     if request.method == 'GET':
         query_text = request.GET.get('query_text')
+        lifter_id = request.GET.get('lifter_id')
 
-        results = Result.objects.annotate(
-            search=SearchVector('lifter__first_name', 'lifter__last_name')
-        ).filter(search=query_text)
+        results = search_for_results(lifter_id)
 
         return render(request, 'public/search.html', {'clubs': clubs, 'results': results})
 
@@ -34,15 +33,9 @@ def search_for_lifter(request):
     :return:
     """
     if request.is_ajax():
-        terms = request.GET.get('term', '')
+        query = request.GET.get('term', '')
 
-        queries = [SearchQuery(query) for query in terms.split(' ')]
-        vector = SearchVector('first_name', 'last_name')
-        query = functools.reduce(operator.or_, queries)
-
-        lifters = Lifter.objects.annotate(
-            rank=SearchRank(vector, query)).order_by('-rank')
-        lifters = lifters.filter(rank__gte=0.04)
+        lifters = search_for_lifter_containing(query)
 
         results = []
         for lifter in lifters:
@@ -63,4 +56,28 @@ def search_for_lifter(request):
     mime_type = 'application/json'
     return HttpResponse(data, mime_type)
 
+# HELPERS
 
+
+def search_for_results(lifter_id):
+    results = Result.objects.filter(lifter_id__exact=lifter_id)
+    return results
+
+
+def search_for_lifters_psql(query):
+    queries = [SearchQuery(x) for x in query.split(' ')]
+    vector = SearchVector('first_name', 'last_name')
+    combined_queries = functools.reduce(operator.or_, queries)
+
+    lifters = Lifter.objects.annotate(
+        rank=SearchRank(vector, combined_queries)).order_by('-rank')
+    lifters = lifters.filter(rank__gte=0.04)
+    return lifters
+
+
+def search_for_lifter_containing(query):
+
+    lifters_first_name = Lifter.objects.filter(first_name__icontains=query)
+    lifters_last_name = Lifter.objects.filter(last_name__icontains=query)
+    lifters = lifters_first_name.union(lifters_last_name)
+    return lifters
