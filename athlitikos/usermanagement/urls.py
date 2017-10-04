ba848e03df6a9b63a53a315aed80aaf55345307c
NoraTomas <noratomas@hotmail.com>
2017-10-04 15:45:04 +0200

    made button clickable

--- athlitikos/usermanagement/urls.py
+++ athlitikos/usermanagement/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^adminPanel/', views.admin, name='home'),
+    url(r'^admin/', views.admin, name='home')
 ]
--- athlitikos/usermanagement/views.py
+++ athlitikos/usermanagement/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import ClubOfficial
 
