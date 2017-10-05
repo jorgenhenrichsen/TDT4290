ae0ae1bdc377d4d830ef20ee0c56f11f0795708e
NoraTomas <noratomas@hotmail.com>
2017-10-05 15:53:57 +0200

    created additional tests

--- athlitikos/resultregistration/tests.py
+++ athlitikos/resultregistration/tests.py
@@ -1,19 +1,43 @@
 from django.test import TestCase
-from .models import Competition
+from .models import Competition, Club
 from .validators import validate_name
-from django.core.exceptions import ValidationError
+from django.test import Client
 
 class CompetitionTestCase(TestCase):
 
     def setUp(self):
-        Competition.objects.create(competitionCategory="Norgesmesterskap", location="Bodø", startDate = "2018-10-05")
-        Competition.objects.create(competitionCategory="NM3", location="Trondheim", startDate = "2019-11-06")
+
+        Competition.objects.create(competitionCategory = "Norgesmesterskap", location = "Bodø", startDate = "2018-10-05")
+        Competition.objects.create(competitionCategory = "Midtnorsk", location = "Trondheim", startDate = "2019-11-06")
+        Club.objects.create(clubName="TrondheimIL", region="Trondheim", address="Arne Bergsgårds Veg 30")
 
     def test_competition_naming_is_valid(self):
 
-        """Competitions with valid competition-categories are identified"""
+        """Competitions with valid naming are identified by using the validate_name method"""
 
         norgesmesterskap = Competition.objects.get(competitionCategory="Norgesmesterskap")
 
         self.assertEqual(validate_name(norgesmesterskap.competitionCategory), "Norgesmesterskap")
+        self.assertEqual(validate_name(norgesmesterskap.location), "Bodø")
+
+    def test_if_one_club_can_join_many_competitions(self):
+
+        """Two competitions are added to a club, to test that the many-to-many relationship works in the database"""
+
+        norgesmesterskap = Competition.objects.get(competitionCategory = "Norgesmesterskap")
+        midtnorsk = Competition.objects.get(competitionCategory = "Midtnorsk")
+        trondheimIL = Club.objects.get(clubName = "TrondheimIL")
+
+        trondheimIL.competition.add(norgesmesterskap, midtnorsk)
+
+        #self.assertEqual(trondheimIL.objects.filter(competition__competitionCategory = "Norgesmesterskap"), "Norgesmesterskap")
+
+
+
+
+
+
+
+
+
 
