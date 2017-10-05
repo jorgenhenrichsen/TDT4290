949096361e05412eb36e5af5fc037a33513e3b0c
NoraTomas <noratomas@hotmail.com>
2017-10-05 14:00:48 +0200

    created our first test

--- athlitikos/resultregistration/tests.py
+++ athlitikos/resultregistration/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Competition
+from .validators import validate_name
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class CompetitionTestCase(TestCase):
+
+    def setUp(self):
+        Competition.objects.create(competitionCategory="Norgesmesterskap", location="Bodø", startDate = "2018-10-05")
+        Competition.objects.create(competitionCategory="NM3", location="Trondheim", startDate = "2019-11-06")
+
+    def test_competition_naming_is_valid(self):
+
+        """Competitions with valid competition-categories are identified"""
+
+        norgesmesterskap = Competition.objects.get(competitionCategory="Norgesmesterskap")
+
+        self.assertEqual(validate_name(norgesmesterskap.competitionCategory), "Norgesmesterskap")
+
--- athlitikos/usermanagement/views.py
+++ athlitikos/usermanagement/views.py
@@ -1,11 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import ClubOfficial
+from django.contrib.auth.decorators import login_required
 
-def club_official_options(request):
-
-    html = ''
-    return HttpResponse()
-
+@login_required(login_url='/login')
 def admin(request):
     return render(request, 'admin.html')
