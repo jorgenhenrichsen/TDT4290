fb033f85624bf544c64167ff72233dfe04ef46c2
Carl O. Steen <carl_steen1@hotmail.com>
2017-10-11 18:34:24 +0200

    better than stash

--- athlitikos/accounts/urls.py
+++ athlitikos/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
+from accounts import views
 
 urlpatterns = [
+    url(r'^adminPanel/', views.admin, name='home'),
+    url(r'^admin/', views.admin, name='home'),
     url(r'', include('resultregistration.urls')),
 ]
--- athlitikos/accounts/views.py
+++ athlitikos/accounts/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def club_official_options(request):
+
+    # html = ''
+    return HttpResponse()
+
+
+@login_required(login_url='/login')
+def admin(request):
+    return render(request, 'admin.html')
