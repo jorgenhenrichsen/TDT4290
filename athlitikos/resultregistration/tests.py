16137278f0ae5ee756e8f7d596384546a62ca4c0
TorMagne <tor.magne@kippersund.no>
2017-10-13 10:02:43 +0200

    changed a competition_dategory to competition_category

--- athlitikos/resultregistration/tests.py
+++ athlitikos/resultregistration/tests.py
@@ -8,7 +8,7 @@ class CompetitionTestCase(TestCase):
     def setUp(self):
 
         Competition.objects.create(competition_category = "Norgesmesterskap", location = "Bodø", start_date = "2018-10-05")
-        Competition.objects.create(competition_dategory = "Midtnorsk", location = "Trondheim", start_date = "2019-11-06")
+        Competition.objects.create(competition_category = "Midtnorsk", location = "Trondheim", start_date = "2019-11-06")
         Club.objects.create(club_name="TrondheimIL", region="Trondheim", address="Arne Bergsgårds Veg 30")
 
     def test_competition_naming_is_valid(self):
