73ea2829cc8447a385bf129f020193a3bef16140
NoraTomas <noratomas@hotmail.com>
2017-10-16 11:33:11 +0200

    changed another URL to english

--- athlitikos/usermanagement/urls.py
+++ athlitikos/usermanagement/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^admin-startside/', views.admin, name='adminpanel'),
+    url(r'^admin-panel/', views.admin, name='adminpanel'),
     url(r'^admin/', views.admin, name='home'),
     url(r'^new-user', views.UserFormView.as_view(), name='register')
 ]
