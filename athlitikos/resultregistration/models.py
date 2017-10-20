0c7742d514b1435e7bc2aba5ce7bddc6ca6e01be
Silje Riseng <silje.riseng@gmail.com>
2017-10-20 09:01:02 +0200

    ran linter

--- athlitikos/resultregistration/models.py
+++ athlitikos/resultregistration/models.py
@@ -160,7 +160,7 @@ class Staff(Person):
 
 
 class PendingResult(Result):
-    
+
     sent = models.BooleanField(verbose_name='Sendt til godkjenning', default=False)
     approved = models.CharField(max_length=20, verbose_name='Status', choices=Status.choices(), default=Status.denied)
 
