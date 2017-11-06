9797ef5bcb9f7aea162898dbcb56e513a65d6f37
Carl O. Steen <carl_steen1@hotmail.com>
2017-11-06 11:33:28 +0100

    atleast two spaces blablabla

--- athlitikos/resultregistration/validators.py
+++ athlitikos/resultregistration/validators.py
@@ -3,7 +3,7 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_name(value):# name can't contain numbers, ect
+def validate_name(value):  # name can't contain numbers, ect
     for v in value:
         if not (v == " " or v == "-" or v.isalpha()):
             raise ValidationError("Ugyldig navn")
